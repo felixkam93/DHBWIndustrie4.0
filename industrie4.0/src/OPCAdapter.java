f8022d461a7a03b9f5632c1105ced092ec0b9d68
Felix Kampsen <kampsen.felix@gmail.com>
2015-03-10 10:50:34 +0100

    kjhsadhf

---
+++ industrie4.0/src/OPCAdapter.java
@@ -0,0 +1,22 @@
+
+public class OPCAdapter implements DataAdapter{
+	
+	public OPCAdapter(){
+		
+	}
+	
+
+
+	@Override
+	public boolean subscribe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unsubscribe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
